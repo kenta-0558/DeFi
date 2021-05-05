ead8efafb862abc0dff5d720f65c4c9ab949f455
kenta0558 <mannheim42@yahoo.de>
2021-05-05 19:29:07 +0200

    add contract owned which has daiToken and owner variables to check osg.sender in modifier onlyOwner

--- DaiFaucet.sol
+++ DaiFaucet.sol
@@ -6,3 +6,19 @@ interface DaiToken {
     function transfer(address dst, uint wad) external returns (bool);
     function balanceOf(address guy) external view returns (uint);
 }
+
+
+contract owned {
+    DaiToken daiToken;
+    address owner;
+
+    constructor() {
+        owner = msg.sender;
+        daiToken = DaiToken(0x4F96Fe3b7A6Cf9725f59d353F723c1bDb64CA6Aa);
+    }
+    
+    modifier onlyOwner {
+        require(msg.sender == owner, "You are not owner of this contract, so you can not call this function");
+        _;
+    }
+}
